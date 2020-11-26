b7ab1312cf4b338df1c03c8403b817bdb72e87a9
Semyon Kondratev <exde.inc@gmail.com>
2020-11-26 19:46:23 +0100

    Sweet refactoring

--- image_processing/src/image_processing.cpp
+++ image_processing/src/image_processing.cpp
@@ -1,58 +1,58 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-using namespace std;
-void show_image(const char* name, cv::Mat cv_matrix, int x_pos, int y_pos, double zoom_factor);
-int main()
+#include "image_processing.h"
+
+int main(int argc, char** argv)
 {
-    cv::Mat image = cv::imread("..//images//fanta.bmp", cv::IMREAD_GRAYSCALE);
-    while (1) {
-        if (image.data == 0) {
-            std::cout << "Image not found\n" << "Press ENTER to quit\n";
-            std::cin.get();
-            return 1;
-        }
-        show_image("Fanta", image, 600, 50, 0.4);
-        //cv::imshow("RGB", image);
-        if ((cv::waitKey(0) & 255) == 'q') 
-        {
-            break;
-        }
-    }
-    return 0;
+	 cv::Mat image = cv::imread("..//images//fanta.bmp", cv::IMREAD_GRAYSCALE);
+
+	 //safety checking
+	 if (image.data == 0) {
+		 std::cout << "Image not found\n" << "Press ENTER to quit\n";
+		 std::cin.get();
+		 return 1;
+	 }
+	 if (image.rows == 0 || image.cols == 0) {
+		 std::cout << "Failure in show_image: height or width are identical to zero\n";
+		 return 2;
+	 }
+
+	 //window properties
+	 auto name = "Image";
+	 auto resized_image = resize_image(image, 0.4);
+
+	 //creating window
+	 cv::namedWindow(name, cv::WINDOW_AUTOSIZE);
+	 cv::moveWindow(name, 600, 50);
+	 while (1) {
+	     cv::imshow(name, resized_image);
+	     if ((cv::waitKey(0) & 255) == 'q') 
+	     {
+	         break;
+	     }
+	 }
+	 return 0;
 }
 
 //enables resizing window while opening
-void show_image(const char* name, cv::Mat image, int x_pos, int y_pos, double zoom_factor) {
+cv::Mat resize_image(cv::Mat image, double zoom_factor) {
+	cv::Mat resized_image = image;
+	cv::Size new_size;
+	
+	//Checking zoom factor
+	if (zoom_factor < 1.e-6)
+	{
+	  std::cout << "Wrong Zoom-factor: too small -> it is set to one\n";
+	  zoom_factor = 1.0;
+	} 
+	int type = image.type();
 
-    if (zoom_factor < 1.e-6)
-    {
-        std::cout << "Wrong Zoom-factor: too small -> it is set to one\n";
-        zoom_factor = 1.0;
-    }
-    cv::namedWindow(name, cv::WINDOW_AUTOSIZE);
-    cv::moveWindow(name, x_pos, y_pos);
-    if (image.rows == 0 || image.cols == 0) {
-        cout << "Failure in show_image: height or width are identical to zero" << endl;
-        return;
-    }
-    int type = image.type();
-    uchar depth = type & CV_MAT_DEPTH_MASK;
-    uchar chans = 1 + (type >> CV_CN_SHIFT);
+	uchar depth = type & CV_MAT_DEPTH_MASK;
+	uchar chans = 1 + (type >> CV_CN_SHIFT);
 
-
-    if (fabs(zoom_factor - 1.0) > 1.e-6) {
-        cv::Mat resized_image;
-        cv::Size new_size;
-        new_size.height = (int)(image.rows * zoom_factor);
-        new_size.width = (int)(image.cols * zoom_factor);
-        cv::resize(image, resized_image, new_size);
-        cv::imshow(name, resized_image);
-    }
-    else
-    {
-        cv::imshow(name, image);
-    }
-
-    return;
+	new_size.height = (int)(image.rows * zoom_factor);
+	new_size.width = (int)(image.cols * zoom_factor);
+	cv::resize(image, resized_image, new_size);
+	return resized_image;
 }
 
